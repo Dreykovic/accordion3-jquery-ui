038b0d1642420d41e2f7404f1bb8dba5ddc51eb5
Birewa Audrey AMONA <122552210+Dreykovic@users.noreply.github.com>
2023-07-09 07:51:04 +0000

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $("#text-js").html("Texte renseigné par le bais de jQuery");
-  $("#submit").click(function() {
-    alert($("#name").val());
-  });
-  $("#a").click(function() {
-    $("#result").load("inc/result.php");
-  });
-});
